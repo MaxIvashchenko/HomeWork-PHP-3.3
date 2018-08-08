9964eafa30e5ffc6ca59f06e7cd536a024411273
MaxIvashchenko <40541533+MaxIvashchenko@users.noreply.github.com>
2018-08-08 14:53:41 +0300

    Update Car.php

--- Productions/Cars/Car.php
+++ Productions/Cars/Car.php
@@ -4,20 +4,20 @@ use Productions\Superclass\Goods;
 
 Class Car extends Goods 
 {
-	public function __construct($name,  $price, $region,$model, $countryIssue, $transmission)
-    {
-    	$this->name = $name;
-    	$this->price = $price;
-    	$this->region = $region;
-        $this->model = $model;
-        $this->countryIssue = $countryIssue;
-        $this->transmission = $transmission;
-    }
+	public $model;
+	public $countryIssue;
+	public $transmission;
+	public function echoCar($model, $countryIssue, $transmission)
+	{
+		$this->model=$model;
+	  	$this->countryIssue=$countryIssue;
+		$this->transmission=$transmission;
+	}	
 	public function getDescription()
-    {
-    echo "<td><b>Название: </b> {$this->name} <br>
-    <b>Марка: </b>{$this->model} <br>
-    <b>Страна производитель: </b>{$this->countryIssue} <br>
-    <b>Коробка передач: </b>{$this->transmission} <br> ";    
-    }
+	{
+	echo "<td><b>Название: </b> {$this->name} <br>
+	<b>Марка: </b>{$this->model} <br>
+	<b>Страна производитель: </b>{$this->countryIssue} <br>
+	<b>Коробка передач: </b>{$this->transmission} <br> ";    
+	}
 }
