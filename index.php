2925f32c30e456c71cbec382f8c4cf68bb6edf58
MaxIvashchenko <40541533+MaxIvashchenko@users.noreply.github.com>
2018-08-08 12:48:42 +0300

    Update index.php

--- index.php
+++ index.php
@@ -14,26 +14,11 @@ if (!empty($_GET['product'])) {
 if (isset($_GET['delete'])) {
     $basket->remove();
 }
-$car1 = new Car('Волга', 12000, 0);
-$car1->putmodel('ГАЗ-3110');
-$car1->putcountryIssue('Россия');
-$car1->puttransmission('механика');
-
-$duck1 = new Duck('Утка', 100, 1);
-$duck1->putage(6);
-$duck1->puthabitat('юг');
- 
-$phone = new Phone('Aplle Iphone 8plus', 1410, 2);
-$phone->putwasMade('Америка');
-$phone->putquality('выше среднего');
-
-$pen1 = new Pen('Ручка Паркер', 10, 1);
-$pen1->putcolor('синий');
-$pen1->putpackAmount(40);
-
-$tv = new Tv('Samsung es06542', 900, 0);
-$tv->putscreenSize('55');
-$tv->puttecnology('4K');
+$car1 = new Car('Волга', 12000, 0, 'ГАЗ-3110', 'Россия', 'механика');
+$duck1 = new Duck('Утка', 100, 1, 6, 'юг');
+$phone = new Phone('Aplle Iphone 8plus', 1410, 2, 'Америка', 'выше среднего');
+$pen1 = new Pen('Ручка Паркер', 400, 1, 'синий', 40);
+$tv = new Tv('Samsung es06542', 900, 0, 55, '4K');
 
 $products = [$car1, $duck1, $phone, $pen1, $tv ];
 ?>
