7c807c56312b48b644a308bf0e811e359f0e3695
MaxIvashchenko <40541533+MaxIvashchenko@users.noreply.github.com>
2018-08-08 14:54:31 +0300

    Update Phone.php

--- Productions/Phones/Phone.php
+++ Productions/Phones/Phone.php
@@ -3,14 +3,13 @@ namespace Productions\Phones;
 use Productions\Superclass\Goods as Goods;
 Class Phone extends Goods
 {	
-	public function __construct($name,  $price, $region, $wasMade, $quality)
-    {
-    	$this->name = $name;
-    	$this->price = $price;
-    	$this->region = $region;
-        $this->wasMade = $wasMade;
-        $this->quality = $quality;
-    }
+	public $wasMade;
+	public $quality;
+	public function echoPhone($wasMade, $quality)
+	{
+		$this->wasMade=$wasMade;
+	  	$this->quality=$quality;
+	}
 	public function getDescription()
     {
     echo "<td><b>Наименование: </b> {$this->name} <br>
